44d2823b79ab14d2008e15a03da6b9f3d9ba284c
Prashanth <prashanth_cvvp@yahoo.co.in>
2015-10-20 17:21:30 -0500

    ContactListApp

--- ContactList/public/script/controllers/loginController.js
+++
@@ -1,8 +0,0 @@
-app.controller('loginController',['$http','$log','$scope',function($http,$log,$scope){
-    var self =this;
-    
-    self.validate=function(){
-        console.log($scope.emailId);
-        console.log($scope.password);
-    };
-}]);
